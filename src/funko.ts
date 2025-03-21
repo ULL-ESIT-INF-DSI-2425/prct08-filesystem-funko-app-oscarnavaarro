79d5fa0b814e6ac577430e294ffdc54c306d60f7
Oscar Navarro <alu0101504094@ull.edu.es>
2025-03-21 20:40:31 +0000

    Creación de la clase Funko

---
+++ src/funko.ts
@@ -0,0 +1,30 @@
+export enum FunkoType {
+  Pop = "Pop!",
+  PopRides = "Pop! Rides",
+  VynilSoda = "Vynil Soda",
+  VynilGold = "Vynil Gold",
+}
+
+export enum FunkoGenre {
+  Animation = "Animación",
+  MoviesTV = "Películas y TV",
+  VideoGames = "Videojuegos",
+  Sports = "Deportes",
+  Music = "Música",
+  Anime = "Anime",
+}
+
+export class Funko {
+  constructor(
+    public id: number,
+    public name: string,
+    public description: string,
+    public type: FunkoType,
+    public genre: FunkoGenre,
+    public franchise: string,
+    public number: number,
+    public exclusive: boolean,
+    public specialFeatures: string,
+    public marketValue: number
+  ) {}
+}
